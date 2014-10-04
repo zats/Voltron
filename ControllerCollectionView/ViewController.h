b039fb5939fafc0cca38aefecfbd64094f8d67ba
Sash Zats <sash@zats.io>
2014-10-04 22:13:40 +0300

    Initial Commit

---
+++ ControllerCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControllerCollectionView
+//
+//  Created by Sash Zats on 10/4/14.
+//  Copyright (c) 2014 Wondermall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ControllerCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ControllerCollectionView
+//
+//  Created by Sash Zats on 10/4/14.
+//  Copyright (c) 2014 Wondermall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
